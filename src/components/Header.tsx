d4670aaf609b59e33ec61cf1e6811223a4c448ef
sam-c-f <sam_friedman@hotmail.co.uk>
2022-11-08 11:19:00 +0000

    setup homepage, header and navbar

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <u>Patient Tracker and Invoice Generator</u>
+      </h1>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home: React.FunctionComponent = () => {
+  return (
+    <React.Fragment>
+      <section>
+        <p>
+          Welcome to my patient tracker and invoice generator. This is
+          specifically designed for medico-legal expert witnesses. It allows new
+          patients and solicitors to be added, and holds them in a database. It
+          then easily allows new invoices to be generated and saves these for
+          accounting.
+        </p>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Home;
